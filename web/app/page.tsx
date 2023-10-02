9a024ff1dd658ebff25a30b2d35041205ac18a92
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-10-02 10:44:34 -0700

    Started flushing out some form logic

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import Start, { StartFormData } from "@/components/start";
+import Survey, { SurveyFormData } from "@/components/survey";
+import { useState } from "react";
+
+enum Step {
+  Start,
+  Survey,
+  Results,
+}
+
 export default function Home() {
-  return <span>Hello world</span>;
+  const [step, setStep] = useState(Step.Start);
+
+  const onStartSubmit = (data: StartFormData) => {
+    console.log(data);
+    setStep(Step.Survey);
+  };
+
+  const onSurveySubmit = (data: SurveyFormData) => {
+    console.log(data);
+    setStep(Step.Survey);
+  };
+
+  return (
+    <div className="flex flex-grow items-center justify-center">
+      {step === Step.Start && <Start onSubmit={onStartSubmit} />}
+      {step === Step.Survey && <Survey onSubmit={onSurveySubmit} />}
+    </div>
+  );
 }
---
+++ web/components/survey.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+
+const schema = z.object({
+  nickname: z.string().nonempty(),
+});
+
+export type SurveyFormData = z.infer<typeof schema>;
+
+interface Props {
+  onSubmit: (data: SurveyFormData) => void;
+}
+
+export default function Survey(props: Props) {
+  const form = useForm<SurveyFormData>({
+    // @ts-ignore Not sure why but this complains about types for no reason
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <div className="flex flex-grow items-center justify-center">
+      <Card className="max-w-sm">
+        <CardHeader>
+          <CardTitle>NATS QCon 2023 Demo</CardTitle>
+          <CardDescription>
+            To kick off the demo, let&apos;s start with a nickname
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(props.onSubmit)}
+              className="space-y-6"
+            >
+              <FormField
+                name="nickname"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nickname</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription>
+                      Use a pseudonym if you&apos;d prefer your name not be in
+                      the talk recording.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Get Started</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
