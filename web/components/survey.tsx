f4fa81a915e5c4ef7cd80454ed547e1d986ea418
Jeremy Saenz <jeremy.saenz@gmail.com>
2023-10-02 11:22:06 -0700

    Survey complete

--- web/components/survey.tsx
+++ web/components/survey.tsx
@@ -21,9 +21,36 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { z } from "zod";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+
+enum Familiarity {
+  None = "I'm a complete newbie",
+  Some = "I've heard of it",
+  Evaluated = "I've used it before",
+  InProduction = "I'm using NATS in production",
+}
+
+enum UseCase {
+  EventStreaming = "Event streaming in NATS",
+  Microservices = "NATS for microservice architectures",
+  IoT = "NATS for IoT, Edge and Fleet management",
+}
+
+enum Industry {
+  Finance = "Finance",
+  Retail = "Retail",
+  Healthcare = "Healthcare",
+  Government = "Government",
+  Entertainment = "Entertainment",
+  Industrial = "Industrial",
+  IoT = "IoT",
+  Other = "Other",
+}
 
 const schema = z.object({
-  nickname: z.string().nonempty(),
+  familiarity: z.nativeEnum(Familiarity),
+  use_case: z.nativeEnum(UseCase),
+  industry: z.nativeEnum(Industry),
 });
 
 export type SurveyFormData = z.infer<typeof schema>;
@@ -42,10 +69,7 @@ export default function Survey(props: Props) {
     <div className="flex flex-grow items-center justify-center">
       <Card className="max-w-sm">
         <CardHeader>
-          <CardTitle>NATS QCon 2023 Demo</CardTitle>
-          <CardDescription>
-            To kick off the demo, let&apos;s start with a nickname
-          </CardDescription>
+          <CardTitle>NATS Survey</CardTitle>
         </CardHeader>
         <CardContent>
           <Form {...form}>
@@ -54,19 +78,99 @@ export default function Survey(props: Props) {
               className="space-y-6"
             >
               <FormField
-                name="nickname"
+                name="familiarity"
                 control={form.control}
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Nickname</FormLabel>
+                    <FormLabel>
+                      How familiar are you with the NATS messaging system?
+                    </FormLabel>
                     <FormControl>
-                      <Input {...field} />
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        {Object.values(Familiarity).map((value) => (
+                          <FormItem
+                            key={value}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem value={value}></RadioGroupItem>
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {value}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
                     </FormControl>
                     <FormMessage />
-                    <FormDescription>
-                      Use a pseudonym if you&apos;d prefer your name not be in
-                      the talk recording.
-                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="use_case"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      What use cases are you interested in learning more about?
+                    </FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        {Object.values(UseCase).map((value) => (
+                          <FormItem
+                            key={value}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem value={value}></RadioGroupItem>
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {value}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="industry"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>What industry do you work in?</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        {Object.values(Industry).map((value) => (
+                          <FormItem
+                            key={value}
+                            className="flex items-center space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <RadioGroupItem value={value}></RadioGroupItem>
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {value}
+                            </FormLabel>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
